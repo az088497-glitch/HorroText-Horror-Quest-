a7f6b46ec88d0045bdc543495c13ebc3c5121449
olga_pansuv <op_2001@inbox.ru>
2026-01-09 17:18:34 +0300

    новая версия игры

--- Project/scr/game_start.py
+++ Project/scr/game_start.py
@@ -8,34 +8,79 @@ with open("story.json", "r", encoding="utf-8") as f:
 
 current_scene = "start"
 player_name = ""
+state = {
+    "refused_journalist": False,
+    "close_seat_v1": False,
+    "fire_alarm_cross2": False,
+    "wait_stay_cross": False,
+}
 
-def show_scene(scene_id):
+def get_choices(scene, mode="default"):
+    choices = scene.get("choices", [])
+
+    if isinstance(choices, dict) and "variants" in choices:
+        return choices["variants"].get(mode, [])
+
+    elif isinstance(choices, list):
+        return choices
+
+    return []
+
+def show_scene(scene_id, mode="default"):
     global current_scene
     current_scene = scene_id
     scene = story[scene_id]
     text_box.config(state ='normal')
     text_box.delete('1.0', tk.END)
     text = scene['text']
-    if '{name}' in text:
-        text = text.replace('{name}', player_name)
     text_box.insert(tk.END, text)
     text_box.config(state='disabled')
+
     for widget in buttons_frame.winfo_children(): # очистка кнопок
         widget.destroy()
 
-    for choice in scene.get("choices", []):
+    choices = get_choices(scene, mode)
+    for choice in choices:
         button = tk.Button(
             buttons_frame,
             text=choice['text'],
             wraplength=400,
-            width=10,
+            width=15,
             command=lambda n=choice["next"]: on_choice(n)
         )
         button.pack(fill='x', pady=5)
+
+    if scene.get("type") == "game_over":
+        restart_button()
+
+
 def on_choice(next_scene):
+    global player_name, state
+
     if next_scene == "introduction" and not player_name:
         ask_name()
-    show_scene(next_scene)
+    elif next_scene == "refuse":
+        state["refused_journalist"] = True
+    elif next_scene == "close_seat_v1":
+        state["close_seat_v1"] = True
+    elif next_scene == "fire_alarm_cross2":
+        state["fire_alarm_cross2"] = True
+    elif next_scene == "wait_stay_cross":
+        state["wait_stay_cross"] = True
+
+    mode = "default"
+    if next_scene == "corridor_cross":
+        if state["refused_journalist"] and state["close_seat_v1"]:
+            mode = "add_var1"
+        elif state["fire_alarm_cross2"]:
+            mode = "add_var2"
+
+    if next_scene == "new_floor_cross":
+        if state["wait_stay_cross"]:
+            mode = "add_var1"
+
+    show_scene(next_scene, mode=mode)
+
 
 def ask_name():
     global player_name
@@ -45,6 +90,15 @@ def ask_name():
     )
     if not player_name:
         player_name = "Player 1"
+
+def restart_button():
+    btn_restart = tk.Button(
+        buttons_frame,
+        text="Пройти заново",
+        width=15,
+        command=lambda: show_scene("start")
+    )
+    btn_restart.pack(fill="x", pady=5)
 
 # Tkinter
 root = tk.Tk()
@@ -60,9 +114,3 @@ buttons_frame.pack(fill="x", padx=10, pady=10)
 show_scene("start")
 
 root.mainloop()
-
-
-
-
-
-
